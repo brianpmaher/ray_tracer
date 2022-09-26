b94d41d82f57c21ad2751e5ff16a9e30a0f29e6f
Brian Maher <bmaher91@gmail.com>
2022-09-26 08:17:17 -0600

    wip - string helper functions

---
+++ code/string.h
@@ -0,0 +1,11 @@
+#pragma once
+
+void StringConcat(char *dest, char *str);
+// {
+//     for (int i = 0; str[i] != '\0'; i++)
+//     {
+//         // TODO: Implement
+//     }
+// }
+
+void AddStringTests(void);
